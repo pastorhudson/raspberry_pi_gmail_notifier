59829387a4246d7b649c5b9236c3e78d42d8f1d7
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2022-01-03 10:19:28 -0500

    Add systemctl install script

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ sudo chmod 644 /lib/systemd/system/email-notifier.service
 sudo systemctl daemon-reload
 sudo systemctl enable email-notifier.service
 sudo systemctl start email-notifier.service
+sudo systemctl status email-notifier.service
