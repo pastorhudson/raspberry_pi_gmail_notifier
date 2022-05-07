ff52b3369ade210d26ed5fc57c6cff481ed2b2f7
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2022-05-07 18:48:52 -0400

    Remove Pyhton version check

--- check_messages_atom.py
+++ check_messages_atom.py
@@ -50,10 +50,10 @@ def set_gpio(status):
 
 if __name__ == '__main__':
 
-    if not (sys.version_info.major == 3 and sys.version_info.minor >= 8):
-        print("This script requires Python 3.8 or higher!")
-        print("You are using Python {}.{}.".format(sys.version_info.major, sys.version_info.minor))
-        sys.exit(1)
+    # if not (sys.version_info.major == 3 and sys.version_info.minor >= 8):
+    #     print("This script requires Python 3.8 or higher!")
+    #     print("You are using Python {}.{}.".format(sys.version_info.major, sys.version_info.minor))
+    #     sys.exit(1)
     service = gmail_authenticate()
     while True:
         try:
